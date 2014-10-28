d8ef696ca8a5d591e3ca4d3f779c7c9f87317b08
michaelbarnes <barnesmike@outlook.com>
2014-10-28 16:58:44 +0200

    Added some stuff

--- scripts/src/search.js
+++ scripts/src/search.js
@@ -8,62 +8,115 @@ iqsElasticAdmin.service('client', function (esFactory) {
 	});
 
 function adminCtrl($scope, client) {
+	var selectedIndex, 
+		selectedItemTypeName, 
+		selectedItemId,
+		documentCount;
 
 	var editor = ace.edit("editor");
 	editor.setTheme("ace/theme/twilight");
 	editor.getSession().setMode("ace/mode/json");
 
-/*	function getIndexEntries(index, type) {
-		client.search({
-		  index: 'schemas',
-		  type: 'schema',
-		}).then(function (resp) {
-			return resp.hits.hits;
-		}, function (err) {
+	getIndicies();
 
-		}
-	};*/
-
-	/*$scope.getIndicies = function () {
-		client.cat.indicies({
-			local: false,
-		}, function (err, response){
-			console.log(response);
+	function getIndicies (){
+		//Indicies scope Variable
+		$scope.indicies = [];
+		//Query to return indicies
+		client.cat.indices({
+			local: false
+			}, function (err, response){
+				if(typeof(response) === "string"){
+					var res = response.match(/(yellow|red|green) [a-z\.]*/g);
+					for(var i = 0; i < res.length; i ++){
+					var item = res[i].split(" ");
+					$scope.indicies.push({name:item[1], status:item[0]});
+				}
+			}
 		});
-	};*/
+	}
 
-	$scope.fetch = function () {
-		client.search({
-		  index: 'schemas',
-		  type: 'schema',
+	//Get the content of the Ace code editor
+	function getEditorContent (){
+
+	}
+
+	//Set the content of the Ace code editor
+	function setEditorContent (jsonDocument){
+		var session = editor.getSession();
+	  	var view = session.setValue(JSON.stringify(jsonDocument, null, 2));
+	}
+
+	function deleteIndexEntry(indexName, typeName, itemId){
+		client.delete({
+			index: indexName,
+			type: typeName,
+			id: itemId
 		}).then(function (resp) {
-		  $scope.hits = resp.hits.hits;
-		  $scope.selectItem = function(schema) {
-		  	var session = editor.getSession();
-		  	var view = session.setValue(JSON.stringify(schema, null, 2));
-		  };
-		}, function (err) {
-		  console.trace(err.message);
-		});
+			console.log(resp);
+		}, function(err) {
+
+		})
 	};
+
+	//Get items in an index
+	function getIndexEntries(indexName) {
+		var hits;
+		selectedIndex = indexName;
+		client.search({
+		  index: indexName
+		}).then(function (resp) {
+			$scope.result = resp;
+			$scope.hits = resp.hits.hits;
+			console.log(resp);
+		}, function (err) {
+
+		})
+	};
+
+	//Get all items within an index
+	$scope.fetchIndexItems = function (index) {
+		getIndexEntries(index.name);
+	};
+
+	//View the content of a document
+	$scope.selectItem = function (hit){
+		selectedItemTypeName = hit._type;
+		selectedItemId = hit._id;
+		setEditorContent(hit._source);
+	};
+
+	$scope.deleteItem = function(hit){
+		alert("You are about to delete a document, are you sure you want to do this?");
+		alert("Are you really sure, please check");
+		deleteIndexEntry(hit._index, hit._type, hit._id);
+
+		getIndicies();
+
+  		getIndexEntries(hit._index);
+	}
 
 	$scope.submitContent = function() {
 		var session = editor.getSession();
 	  	var value = session.getValue();
 	  	var deserializedJson = angular.fromJson(value);
-	  	var documentId = deserializedJson.id;
-	  	console.log(documentId);
-
 	  	client.index({
-	  		index: 'schemas',
-	  		type: 'schema',
-	  		id: documentId,
+	  		index: selectedIndex,
+	  		type: selectedItemTypeName,
+	  		id: selectedItemId,
 	  		body: deserializedJson
 	  	}, function (err, response) {
-	  		if(err == undifined){
+	  		if(err === "undifined"){
+
 	  			alert("There was an error saving the document: \r"+ err.message);
 	  		}
+	  		console.log(response);
+
 	  		alert("Response from server: \rindex: " + response._index + "\rtype: " + response._type + "\rid: " + response._id + "\rversion: " + response._version);
+
+			getIndicies();
+
+	  		getIndexEntries(selectedIndex);
 	  	});
 	};
 };
